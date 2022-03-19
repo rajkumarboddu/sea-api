345be521900bbcff7a24a21b3c77b5734ea82615
Raj <rboddu@zustsign.com>
2022-03-19 21:42:02 +0530

    Remove unused files.

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/vehicle', vehicleRouter);
 app.use('/user', userRouter);
@@ -35,7 +34,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({ 'msg': 'Something went wrong!' });
 });
 
 module.exports = app;
